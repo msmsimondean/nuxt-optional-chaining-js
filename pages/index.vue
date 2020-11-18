a1935f8b114ae12c15c31981cb085e79eed82c79
Simon Dean <simon.dean@moneysupermarket.com>
2020-11-18 17:46:06 +0000

    Use optional chaining in template

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,19 @@
 <template>
   <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        nuxt-optional-chaining-js
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    {{ hello.world?.greeting }}
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      hello: {
+
+      }
+    }
+  }
+}
 </script>
 
 <style>
